d21873a170173d25fa58bebd879217e21595033d
Eduardo Oliveira <eduardoykky@gmail.com>
2022-10-27 17:21:32 -0300

    first commit

---
+++ 4listaarray4.js
@@ -0,0 +1,24 @@
+/*
+Faça um algoritmo que leia um array, e o preencha com números inteiros, se esse
+array tiver alguma posição com valor zero você deve eliminar essa posição. Para
+fazer isso, todos os elementos à frente do valor zero devem ser movidos uma
+posição para trás no vetor.
+Ex.: entrada - array[2,6,8,0,6,4,3,2,0,7]
+ saída - array[2,6,8,6,4,3,2,7]
+
+*/
+var arrayA = []
+var arraySuporte = []
+var indexSup = 0
+for (var index = 0; index < 5; index++) {
+    arrayA[index] = parseInt(prompt("Insira um número."))
+}
+console.log(arrayA)
+for (var index = 0; index < 5; index++) {
+    if (arrayA[index] != 0) {
+       arraySuporte[indexSup] = arrayA[index]
+       indexSup++
+    }
+}
+arrayA = arraySuporte
+console.log(arrayA)
